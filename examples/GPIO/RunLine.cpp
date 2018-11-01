4a4109f84cce2dfd5c699256432a1bb07f7b654d
eric <wenxiangdong@outlook.com>
2018-11-01 09:26:44 +0800

    save

--- examples/GPIO/RunLine.cpp
+++ examples/GPIO/RunLine.cpp
@@ -5,51 +5,20 @@ using namespace GPIO;
 int main()
 {
 	init();
-	for (int i=1;i<=10;i++){
-		controlLeft(FORWARD,60);
-		controlRight(FORWARD,60);
+	for (int i=1;i<=12;i++){
+
+		if (i&1){
+			controlLeft(FORWARD,15);
+			controlRight(FORWARD,15);
+		}else{
+			controlRight(FORWARD,15);
+			controlLeft(FORWARD,15);
+		}
 		delay(200);
 	}
 	stopLeft();
 	stopRight();
 	return 0;
-	//Move forward
-	controlLeft(FORWARD,50);
-	controlRight(FORWARD,50);
-	delay(1000);
 
-	//Stop
-	stopLeft();
-	stopRight();
-	delay(1000);
 
-	//Move backward
-	controlLeft(BACKWARD,50);
-	controlRight(BACKWARD,50);
-	delay(1000);
-
-	//Stop
-	stopLeft();
-	stopRight();
-	delay(1000);
-
-	//2 motors can work at different speeds.
-	controlLeft(FORWARD,30);
-	controlRight(FORWARD,40);
-	delay(1000);
-
-	//Stop
-	stopLeft();
-	stopRight();
-	delay(1000);
-
-	//Even directions can differ from each other.
-	controlLeft(BACKWARD,35);
-	controlRight(FORWARD,20);
-	delay(1000);
-
-	//Don't forget to stop all motors before exiting.
-	stopLeft();
-	stopRight();
-	return 0;
 }
--- examples/GPIO/run.sh
+++ examples/GPIO/run.sh
@@ -1,1 +1,2 @@
 g++ -Wall -o $1 $1.cpp -lwiringPi -lGPIO -L ../../lib/libGPIO.a
+sudo ./$1
